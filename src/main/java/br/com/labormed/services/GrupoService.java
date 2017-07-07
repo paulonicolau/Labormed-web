80c287411577977cefdf1f77461637cd9da79a3a
paulonicolau <paulonicolau@oi.com.br>
2017-07-07 09:00:36 -0300

    .

--- src/main/java/br/com/labormed/services/GrupoService.java
+++ src/main/java/br/com/labormed/services/GrupoService.java
@@ -1,9 +1,6 @@
 package br.com.labormed.services;
 
-import java.util.List;
-
 import br.com.labormed.model.Grupo;
-import br.com.labormed.model.SubGrupo;
 
 public interface GrupoService extends Service<Grupo>{
 }
